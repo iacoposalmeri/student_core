f7a20691a8e46f698237aaef26ded1bcc93070da
Lorenzo129 <lorenzoamato1117@gmail.com>
2026-05-17 12:19:15 +0200

    Creazione Login

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false
+})
+export class LoginPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  accedi() {
+    console.log("Tentativo di accesso in corso...");
+    this.router.navigate(['/home']);
+  }
+}
