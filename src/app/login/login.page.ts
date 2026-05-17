a1610c14550460d2304df552742b0d80333e5b32
Lorenzo129 <lorenzoamato1117@gmail.com>
2026-05-17 14:55:30 +0200

    Login modificato

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -9,13 +9,50 @@ import { Router } from '@angular/router';
 })
 export class LoginPage implements OnInit {
 
+  emailInput: string = '';
+  passwordInput: string = '';
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off-outline';
+  isLoading: boolean = false;
+
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  isEmailValid(): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@community\.unipa\.it$/;
+    return emailRegex.test(this.emailInput);
+  }
+
+  isPasswordValid(): boolean {
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    return passwordRegex.test(this.passwordInput);
+  }
+
+  isFormValid(): boolean {
+    return this.isEmailValid() && this.isPasswordValid();
+  }
+
+  togglePassword() {
+    if (this.passwordType === 'password') {
+      this.passwordType = 'text';
+      this.passwordIcon = 'eye-outline';
+    } else {
+      this.passwordType = 'password';
+      this.passwordIcon = 'eye-off-outline';
+    }
+  }
+
   accedi() {
-    console.log("Tentativo di accesso in corso...");
-    this.router.navigate(['/home']);
+    if (!this.isFormValid()) return;
+
+    this.isLoading = true; 
+    console.log("Accesso Istituzionale in corso...");
+    
+    setTimeout(() => {
+      this.isLoading = false; 
+      this.router.navigate(['/home']); 
+    }, 1500);
   }
 }
