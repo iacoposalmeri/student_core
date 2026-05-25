8ba09322ad8f6f9bd974ebeba107de52dbc80091
Lorenzo129 <lorenzoamato1117@gmail.com>
2026-05-25 13:38:55 +0200

    Tab News completata

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-news',
@@ -8,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPage implements OnInit {
 
-  constructor() { }
+  newsList: any[] = [];
+  isLoading: boolean = true;
+  idStudente: string | null = null; 
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.idStudente = localStorage.getItem('id');
+    this.caricaNews();
   }
 
+  ionViewWillEnter() {
+    if (!this.idStudente) {
+      this.idStudente = localStorage.getItem('id');
+    }
+    this.caricaNews();
+  }
+
+  caricaNews() {
+    if (!this.idStudente) return;
+    
+    this.isLoading = true;
+    this.http.get<any[]>(`http://localhost:3000/api/news/studente/${this.idStudente}`).subscribe({
+      next: (data) => {
+        this.newsList = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error("Errore durante il caricamento delle news:", err);
+        this.isLoading = false;
+      }
+    });
+  }
+
+  doRefresh(event: any) {
+    if (!this.idStudente) {
+      event.target.complete();
+      return;
+    }
+
+    this.http.get<any[]>(`http://localhost:3000/api/news/studente/${this.idStudente}`).subscribe({
+      next: (data) => {
+        this.newsList = data;
+        event.target.complete();
+      },
+      error: (err) => {
+        event.target.complete();
+      }
+    });
+  }
 }
