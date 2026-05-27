46336c4fc07342606fead9df6d74d140c913ba81
Iacopo Salmeri <iacoposalmeri@gmail.com>
2026-05-27 12:15:41 +0200

    modifiche varie

--- src/app/carriera/carriera.page.ts
+++ src/app/carriera/carriera.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-carriera',
@@ -8,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrieraPage implements OnInit {
 
-  constructor() { }
+  esamiSuperati: any[] = [];
+
+  totaleCfu : number = 0;
+  mediaVoti: number = 0;
+  votoLaurea: number = 0;
+
+  idStudente : string | null = null;
+
+  isLoadingEsami: boolean = true;
+
+  constructor(
+    private http: HttpClient, 
+  ) { }
 
   ngOnInit() {
+    this.idStudente = localStorage.getItem('id');
+
+    this.http.get<any[]>("http://localhost:3000/api/esami/" + this.idStudente).subscribe({
+      next: (data) => {
+        this.esamiSuperati = data;
+        this.calcolaStatistiche();
+        this.isLoadingEsami = false;
+      },
+      error: (err) => {
+        console.error("Errore esami:", err);
+        this.isLoadingEsami = false;
+      }
+    });
+    
+  }
+
+  calcolaStatistiche() {
+    this.totaleCfu = 0;
+    this.mediaVoti = 0;
+    var sommaPonderata = 0;
+
+    this.esamiSuperati.forEach(esame => {
+      this.totaleCfu += esame.cfu;
+      sommaPonderata += (esame.voto * esame.cfu);
+    })
+    if (this.totaleCfu>0) {
+      this.mediaVoti = sommaPonderata / this.totaleCfu;
+      this.votoLaurea = this.mediaVoti / 30 * 110;
+    }
   }
 
 }
