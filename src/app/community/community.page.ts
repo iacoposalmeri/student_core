19d5d78e019c0562a352e5d148781e4aee46d4d3
Iacopo Salmeri <iacoposalmeri@gmail.com>
2026-06-05 12:54:08 +0200

    community

--- src/app/community/community.page.ts
+++ src/app/community/community.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-community',
@@ -8,9 +10,122 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommunityPage implements OnInit {
 
-  constructor() { }
+  statoCommunity: string = 'campus';
+  idStudente: string | null = null;
+  
+  annunci: any[] = [];
+
+  modelloAnnuncio: any = {
+    titolo: null,
+    descrizione: null,
+    prezzo: null,
+    tipologia: null,
+    data_pubblicazione: null,
+    id_studente: null
+  };
+
+  servizi: any[] = [];
+  aule: any[] = [];
+  testoRicercaAula: string = '';
+
+  isModalAperto: boolean = false;
+
+  get auleFiltrate() {
+    if (!this.testoRicercaAula) {
+      return this.aule;
+    }
+    const testo = this.testoRicercaAula.toLowerCase();
+    return this.aule.filter(a => 
+      a.nome_aula.toLowerCase().includes(testo) || 
+      a.edificio.toLowerCase().includes(testo)
+    );
+  }
+
+  constructor(private http: HttpClient, private toastController: ToastController) { }
 
   ngOnInit() {
+    this.idStudente = localStorage.getItem('id');
+    this.getAnnunci();
+    this.getAule();
+    this.getServizi();
+  }
+
+  getAnnunci() {
+    this.http.get<any[]>("http://localhost:3000/api/annunci").subscribe({
+      next: (data) => {
+        this.annunci = data;
+      },
+      error: (err) => console.error("Errore annunci:", err)
+    });
+  }
+
+  postAnnunci() {
+    this.modelloAnnuncio.id_studente = this.idStudente;
+    const ora = new Date();
+    const dataFormattata = ora.toLocaleDateString('it-IT') + ', ' + ora.toLocaleTimeString('it-IT', { hour: '2-digit', minute: '2-digit' });
+    this.modelloAnnuncio.data_pubblicazione = dataFormattata;
+
+    this.http.post("http://localhost:3000/api/annunci", this.modelloAnnuncio).subscribe({
+      next: () => {
+        this.getAnnunci();
+        this.modelloAnnuncio = {
+          titolo: null,
+          descrizione: null,
+          prezzo: null,
+          tipologia: null,
+          data_pubblicazione: null,
+          id_studente: null
+        };
+      },
+      error: (err) => console.error("Errore caricamento annuncio:", err)
+    });
+  }
+
+
+  
+  getServizi() {
+    this.http.get<any[]>("http://localhost:3000/api/campus/servizi").subscribe({
+      next: (data) => {
+        this.servizi = data;
+      },
+      error: (err) => console.error("Errore servizi:", err)
+    });
+  }
+
+  getAule() {
+    this.http.get<any[]>("http://localhost:3000/api/campus/aule").subscribe({
+      next: (data) => {
+        this.aule = data;
+      },
+      error: (err) => console.error("Errore aule:", err)
+    });
+  }
+
+  async postAule(idAula: number, livello: string) { 
+    
+    const dataFormattata = new Date().toISOString();
+    var pacchetto: any = {
+      id_studente : this.idStudente,
+      id_aula: idAula,
+      data_ora : dataFormattata,
+      livello_affollamento: livello, 
+    };
+
+    this.http.post("http://localhost:3000/api/campus/checkin", pacchetto).subscribe({
+      next: async () => {
+        const toast = await this.toastController.create({
+          message: `Grazie! Segnalazione effettuata: Affollamento ${livello}`,
+          duration: 2500,
+          color: 'success',
+          position: 'bottom',
+          icon: 'checkmark-circle-outline'
+        });
+        await toast.present();
+
+        this.getAule();
+      },
+      error: (err) => console.error("Errore caricamento aule:", err)
+    });
   }
 
 }
