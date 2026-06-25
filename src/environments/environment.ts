9d4c0575887ee025e9d2e88e6afccd9f1f2e495f
Iacopo Salmeri <iacoposalmeri@gmail.com>
2026-06-25 18:14:07 +0200

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://10.83.60.213:3000'
 };
 
 /*
